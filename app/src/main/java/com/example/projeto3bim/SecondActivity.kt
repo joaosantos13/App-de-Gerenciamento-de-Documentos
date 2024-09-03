d3dcb9e0b4469dad955ee86f0c19d517bd8140f2
João Santos <jviniciussantos13@gmail.com>
2024-09-03 19:56:26 -0300

    login_e_registro_feito

--- app/src/main/java/com/example/projeto3bim/SecondActivity.kt
+++ app/src/main/java/com/example/projeto3bim/SecondActivity.kt
@@ -1,74 +1,42 @@
 package com.example.projeto3bim
 
-import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var nomeEditText: EditText
+class SecondActivity : AppCompatActivity()
+{
+    private lateinit var loginButton: Button
     private lateinit var emailEditText: EditText
     private lateinit var passwordEditText: EditText
-    private lateinit var registerButton: Button
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
 
-        // Write a message to the database
         val database = FirebaseDatabase.getInstance()
         val myRef = database.getReference("message")
 
         FirebaseApp.initializeApp(this)
-        nomeEditText = findViewById(R.id.nomeTextview)
+
+        loginButton = findViewById(R.id.loginButton)
         emailEditText = findViewById(R.id.emailTextview)
         passwordEditText = findViewById(R.id.senhaTextview)
-        registerButton = findViewById(R.id.enviarButton)
 
-        registerButton.setOnClickListener {
+        loginButton.setOnClickListener {
 
             val email = emailEditText.text.toString().trim()
             val password = passwordEditText.text.toString().trim()
 
-            register(email, password)
+            login(email, password)
 
         }
 
-        val buttonLink = findViewById<TextView>(R.id.button_link)
-        buttonLink.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
-            startActivity(intent)
-        }
-
-
-    }
-
-    private fun register(email: String, password: String)
-    {
-        val auth = FirebaseAuth.getInstance()
-        auth.createUserWithEmailAndPassword (email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                // Registro bem-sucedido
-                val user = auth.currentUser
-                Toast.makeText(this, "Registrado com sucesso: ${user?.email}", Toast.LENGTH_SHORT).show()
-                } else {
-                    // Falha no registro
-                    Toast.makeText(
-                        this,
-                        "Erro ao registrar: ${task.exception?.message}",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
     }
 
     fun login(email: String, password: String) {
@@ -91,4 +59,6 @@ class MainActivity : AppCompatActivity() {
                 }
             }
     }
+
 }
+
